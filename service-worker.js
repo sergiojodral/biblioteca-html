9c8fb25767a4aef6176b940c1b0a7d131723ebb8
sergiojodral <sergiojodral@gmail.com>
2025-12-30 14:58:24 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'biblioteca-html-v1';
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './activities.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
